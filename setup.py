aabd7750cb770019044310d01a3b6d83a9fd11ca
Thomas Levine <_@thomaslevine.com>
2014-03-31 21:43:50 +0000

    setup.py

---
+++ setup.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+from distutils.core import setup
+
+from blizzard import __version__
+
+setup(name='blizzard',
+    author = 'Thomas Levine',
+    author_email = '_@thomaslevine.com',
+    description = 'Find the unique indices for a ton of datasets',
+    url = 'https://github.com/tlevine/blizzard.git',
+    classifiers = [
+        'Intended Audience :: Developers',
+    ],
+    packages = ['blizzard'],
+    install_requires = ['special_snowflake'],
+    tests_require = ['nose'],
+    version = __version__,
+    license = 'AGPL',
+)
