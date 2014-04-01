2b09a583b2ca564773f80af9da12aa1a16eba9da
Thomas Levine <_@thomaslevine.com>
2014-04-01 15:07:39 +0000

    crazyness graphing

--- blizzard/graph.py
+++ blizzard/graph.py
@@ -4,4 +4,13 @@ class Graph(nx.Graph):
     def add_dataset(self, dataset):
         dataset_id = (dataset['catalog'], dataset['datasetid'])
         for unique_index in dataset['unique_indices']:
-            self.add_edge(tuple(sorted(unique_index)),dataset_id)
+            i = tuple(sorted(unique_index))
+            self.add_node(i, kind = 'index')
+            self.add_node(dataset_id, kind = 'dataset', text = dataset['download'].text)
+            self.add_edge(i, dataset_id)
+            for neighbor_datasetid in self.neighbors(i, data = True):
+                j = tuple(sorted(neighbor_datasetid, datasetid))
+                if neighbor_datasetid != datasetid and j not in self.edges_iter():
+                   texts = nx.get_node_attributes(self, 'text')
+                   attr = overlap(texts['neighbor_datasetid'], texts['datasetid'])
+                   self.add_edge(neighbor_datasetid, datasetid, attr)
