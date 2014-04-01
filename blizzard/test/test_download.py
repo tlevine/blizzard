01287221c640307508550387fd517d546b2d8df5
Thomas Levine <_@thomaslevine.com>
2014-04-01 13:29:15 +0000

    fix test

--- blizzard/test/test_download.py
+++ blizzard/test/test_download.py
@@ -21,7 +21,7 @@ def test_run():
     def get(url):
         return r
     observed = list(blizzard.download._run(get, 'aoeu'))
-    expected = [{'datasetid':3,'download':r}]
+    expected = [{'datasetid':3,'catalog':'aoeu','download':r}]
     n.assert_list_equal(observed, expected)
 
 def test_all():
