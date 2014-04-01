630d5f46dfb391d765e00c1e1df8aa45d684f3cb
Thomas Levine <_@thomaslevine.com>
2014-04-01 13:09:35 +0000

    -

--- blizzard/test/test_download.py
+++ blizzard/test/test_download.py
@@ -1,15 +1,25 @@
 from collections import namedtuple
+import json
 
 import nose.tools as n
 
 import blizzard.download
 
+Response = namedtuple('Response', ['text'])
+
 def test_datasets():
     def get(url):
-        Response = namedtuple('Response', ['text'])
         return Response(text = '{"datasets":[{"foo":8}]}')
     catalog = 'stnoheustahoe'
 
     observed = blizzard.download.datasets(get, catalog)
     expected = [{'foo':8,'catalog':catalog}]
+    n.assert_list_equal(observed, expected)
+
+def test_run():
+    datasets = [{'datasetid':3}]
+    def get(url):
+        return Response(text = json.dumps({'datasets':datasets}))
+    observed = list(blizzard.download._run(get, 'aoeu'))
+    expected = datasets * 30
     n.assert_list_equal(observed, expected)
