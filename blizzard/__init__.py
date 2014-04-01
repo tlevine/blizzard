d97fa76dc95f392936132ee87606b58de55a83ec
Thomas Levine <_@thomaslevine.com>
2014-04-01 13:37:35 +0000

    error

--- blizzard/__init__.py
+++ blizzard/__init__.py
@@ -25,15 +25,19 @@ def main():
     get = functools.partial(_get, datadir)
     for dataset in download.all(get):
         logger.debug('%s, %s: Got metadata' % (dataset['catalog'], dataset['datasetid']))
-        response = download.download(get, dataset['catalog'], dataset['datasetid'])
         logger.debug('%s, %s: Got spreadsheet' % (dataset['catalog'], dataset['datasetid']))
 
         if ignore(dataset):
             logger.debug('%s, %s: Skipping' % (dataset['catalog'], dataset['datasetid']))
         else:
             logger.debug('%s, %s: Snowflaking' % (dataset['catalog'], dataset['datasetid']))
-            with StringIO(response.text) as fp:
-                dataset['unique_indices'] = fromcsv(fp, delimiter = ';', n_columns = n_columns)
+            with StringIO(dataset['download'].text) as fp:
+                try:
+                    dataset['unique_indices'] = fromcsv(fp, delimiter = ';', n_columns = n_columns)
+                except:
+                    logger.error('%s, %s: Error' % (dataset['catalog'], dataset['id']))
+                    dataset['unique_indices'] = set()
+
             logger.debug('%s, %s: Found indices %s' % (dataset['catalog'], dataset['datasetid'], dataset['unique_indices']))
             g.add_dataset(dataset)
 
