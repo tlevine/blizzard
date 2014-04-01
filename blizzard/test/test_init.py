5f89ce027269b6c7f4db61a4b7d30545ef78d0bf
Thomas Levine <_@thomaslevine.com>
2014-04-01 13:56:01 +0000

    -

--- blizzard/test/test_init.py
+++ blizzard/test/test_init.py
@@ -6,7 +6,7 @@ from blizzard import _snow
 
 def test_snow():
     with open(os.path.join('blizzard','test','fixture','budget-credits_de_paiement.p'),'rb') as fp:
-        dataset = {'datasetid':8,'catalog':'foo','download':pickle.load(fp)}
+        dataset = {'datasetid':8,'catalog':'foo','fields':[],'download':pickle.load(fp)}
     observed = _snow(dataset)['unique_indices']
     expected = set()
     n.assert_set_equal(observed, expected)
