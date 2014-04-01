cc789de9bfcc1d305bb53b406e62a77a641b5486
Thomas Levine <_@thomaslevine.com>
2014-04-01 12:41:34 +0000

    -

--- blizzard/main.py
+++ blizzard/main.py
@@ -1,10 +1,15 @@
+import functools
+import pickle
+from io import StringIO
+import os
+
 import pickle_warehouse
 
 from blizzard.graph import Graph
 import blizzard.download as download
 
-def get(load:bool, url:str):
-    warehouse = pickle_warehouse.Warehouse(os.path.expanduser('~/dadawarehouse.thomaslevine.com/opendatasoft'))
+def _get(datadir:str, load:bool, url:str):
+    warehouse = pickle_warehouse.Warehouse(datadir)
     if url in warehouse:
         response = warehouse[url]
     else:
@@ -16,13 +21,16 @@ def ignore(dataset):
     return 'geo_shape' in set(f['type'] for f in dataset['fields'])
 
 def main():
+    n_columns = 3
+    datadir = os.path.expanduser('~/dadawarehouse.thomaslevine.com/opendatasoft')
+
     g = Graph()
+    get = functools.partial(_get, datadir)
     for dataset in download.all(get):
         response = download.download(get, dataset['catalog'], dataset['datasetid'])
         if not ignore(dataset):
             with StringIO(response.text) as fp:
-                dataset['unique_indices'] = unique_indices(fp, url)
+                dataset['unique_indices'] = fromcsv(fp, delimiter = ';', n_columns = n_columns)
             g.add_dataset(dataset)
     with open('graph.p', 'wb') as fp:
         pickle.dump(g, fp)
-
