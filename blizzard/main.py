51d2114b7a73887c41cd68d48735d4fadb84661a
Thomas Levine <_@thomaslevine.com>
2014-04-23 20:14:06 +0000

    -

--- blizzard/main.py
+++ blizzard/main.py
@@ -1,15 +1,19 @@
+import logging
+import functools
 
-datadir = os.path.expanduser('~/dadawarehouse.thomaslevine.com/big/opendatasoft')
-n_workers = 30
-
-# Logging
-fp_log = logging.FileHandler('blizzard.log', 'w')
-logger.setLevel(logging.DEBUG)
-fp_log.setLevel(logging.DEBUG)
-logger.addHandler(fp_log)
-logger.debug('Starting a new run\n==============================================')
+from blizzard.nxgraph import Graph
 
 def main():
+    datadir = os.path.expanduser('~/dadawarehouse.thomaslevine.com/big/opendatasoft')
+    n_workers = 30
+
+    # Logging
+    fp_log = logging.FileHandler('blizzard.log', 'w')
+    logger.setLevel(logging.DEBUG)
+    fp_log.setLevel(logging.DEBUG)
+    logger.addHandler(fp_log)
+    logger.debug('Starting a new run\n==============================================')
+
     get = functools.partial(_get, datadir)
     for catalog in catalogs:
         def f(dataset):
@@ -17,3 +21,9 @@ def main():
             return dataset
         for future in jumble(f, datasets(get, catalog), n_workers):
             yield future.result()
+
+def metadata(dataset_text):
+    with StringIO(dataset_text) as fp:
+        r = csv.DictReader(fp, delimiter = ';')
+        nrow = ilen(r)
+    return {'nrow': nrow}
--- blizzard/real_work.py
+++ blizzard/real_work.py
@@ -1,8 +1,3 @@
 from logging import getLogger
 logger = getLogger('blizzard')
 
-def metadata(dataset_text):
-    with StringIO(dataset_text) as fp:
-        r = csv.DictReader(fp, delimiter = ';')
-        nrow = ilen(r)
-    return {'nrow': nrow}
