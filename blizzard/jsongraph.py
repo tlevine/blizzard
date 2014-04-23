88eb0ff59c151755fc36d1d7771c234aa44658b7
Thomas Levine <_@thomaslevine.com>
2014-04-23 16:48:00 +0000

    debug

--- blizzard/jsongraph.py
+++ blizzard/jsongraph.py
@@ -4,7 +4,9 @@ from itertools import combinations
 
 def write_triple(fp, subject, predicate, object):
     row = (subject, predicate, object)
-    fp.write(json.dumps(row) + '\n')
+    line = json.dumps(row) + '\n'
+    fp.write(line)
+    print(line)
 
 def add_dataset(metadata, fp, dataset):
     '''
@@ -15,8 +17,11 @@ def add_dataset(metadata, fp, dataset):
     dataset_url = 'http://%s/explore/dataset/%s/' % (dataset['catalog'], dataset['datasetid'])
     self.add_node(dataset_id, kind = 'dataset')
     save = functools.partial(write_triple, fp, dataset_url)
+    print(3)
     for unique_index in dataset['unique_indices']:
+        print(4)
         i = tuple(sorted(unique_index))
         save('unique index', i)
     for k, v in metadata(dataset['download'].text):
+        print(5)
         save(k, v)
