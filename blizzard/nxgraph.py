a2fcc84e207be90fea2f39dc45d3b7580cb2d3f0
Thomas Levine <_@thomaslevine.com>
2014-04-23 21:52:46 +0000

    pickle

--- blizzard/nxgraph.py
+++ blizzard/nxgraph.py
@@ -1,4 +1,5 @@
 from itertools import combinations
+import json, pickle
 
 import networkx as nx
 
@@ -39,6 +40,6 @@ def main():
         for line in fp:
             g.add_dataset(json.loads(line))
     with open('blizzard.p', 'wb') as fp:
-        fp.write(g)
+        pickle.dump(g, fp)
 
 main()
