477b495fef348e7fa60c73e3918d557dde9fa061
Thomas Levine <_@thomaslevine.com>
2014-04-24 21:00:46 +0000

    more metadata

--- blizzard/meta.py
+++ blizzard/meta.py
@@ -13,14 +13,22 @@ def individual_metadata(dataset_text):
     with StringIO(dataset_text) as fp:
         r = csv.DictReader(fp, delimiter = ';')
         nrow = ilen(r)
+        fp.seek(0)
+        column_names = next(r)
     with StringIO(dataset_text) as fp:
         unique_keys = list(sorted(fromcsv(fp, delimiter = ';')))
-    return {'nrow': nrow, 'unique_keys': unique_keys}
+    return {
+        'nrow': nrow,
+        'unique_keys': unique_keys,
+        'column_names': column_names,
+    }
 
 def pairwise_metadata(unique_key, left_text, right_text):
     left_hashes = _index_hashes(unique_key, left_text)
     right_hashes = _index_hashes(unique_key, right_text)
-    return len(left_hashes.intersection(right_hashes))
+    return {
+        'nrow_overlap': len(left_hashes.intersection(right_hashes)),
+    }
 
 def _index_hashes(unique_key, dataset_text):
     hashes = set()
